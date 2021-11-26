4abf5e09e35b9d7c83dca1f3e0dfca655291de51
vdtdg <valerian@dtdg.fr>
2021-11-26 23:34:34 +0100

    Configured back-end with cors and csp. Also added a simple index to front-end

--- backend.js
+++ backend.js
@@ -1,46 +1,29 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
+const cors = require('cors');
+const csp = require('helmet-csp')
 
+// Definition of application options
 const app = express();
-
-const soundList = {
-    "a_kadoc": {
-        "id": 1,
-        "file_name": "a_kadoc",
-        "path": "sounds/a_kadoc.mp3",
-        "quote": "À Kadoc!",
-        "author": "Kadoc",
-        "season": "2",
-        "episode_number": "60",
-        "episode_name": "Le jeu du caillou"
-    },
-    "a_la_volette1": {
-        "id": 2,
-        "file_name": "a_la_volette1",
-        "path": "sounds/a_la_volette1.mp3",
-        "quote": "À la volette",
-        "author": "Arthur",
-        "season": "1",
-        "episode_number": "26",
-        "episode_name": "À la volette"
-    },
-    "2_3_poils_de_Q": {
-        "id": 3,
-        "file_name": "2_3_poils_de_Q",
-        "path": "sounds/2_3_poils_de_Q.mp3",
-        "quote": "À 2-3 poils de cul",
-        "author": "Léodagan",
-        "season": "1",
-        "episode_number": "88",
-        "episode_name": "L'escorte"
+cspOptions = {
+    useDefaults: true,
+    directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'"]
     }
 }
+app.use(cors());
+app.use(csp(cspOptions))
 
+// Loading all the sounds data
+const soundList = require('./sounds/sounds.json');
+
+// Declaring routes
 app.get('/sound-list', (req, res) => {
+    res.header('Content-Type', 'application/json');
     res.send(soundList);
 })
-
 
 app.get('/sound/:id', (req, res) => {
     // First read existing users.
@@ -50,13 +33,13 @@ app.get('/sound/:id', (req, res) => {
     res.writeHead(200, {
         'Content-Type': 'audio/mpeg',
         'Content-Length': stat.size,
-
     });
 
     const readStream = fs.createReadStream(filePath);
     readStream.pipe(res);
 })
 
+// Starting the app on port 8081
 const server = app.listen(8081, function () {
     const host = server.address().address
     const port = server.address().port
